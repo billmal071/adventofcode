37129414212d220c086a80e4758b4716c31eda98
Williams Malachy <billmal071@gmail.com>
2024-12-13 22:49:53 +0100

    Just completed day 1

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 // use crate::y2023::day1::day1_part2::trebuchet;
 // use crate::y2023::day2::day2_part1::cube_conundrum;
 // use crate::y2023::day2::day2_part2::cube;
-use crate::y2024::day1::day1_part1::total_distance_btw_list;
+// use crate::y2024::day1::day1_part1::total_distance_btw_list;
+use crate::y2024::day1::day1_part2::similarity_score;
 
 // mod y2023;
 mod y2024;
@@ -12,5 +13,6 @@ fn main() {
   // trebuchet();
   // cube_conundrum();
   //cube();
-  total_distance_btw_list();
+  // total_distance_btw_list();
+  similarity_score();
 }
---
+++ src/y2024/day1/day1_part2.rs
@@ -0,0 +1,50 @@
+use std::{collections::HashMap, fs};
+
+pub fn similarity_score() -> u32 {
+    let mut first_part = Vec::new();
+    let mut second_part = Vec::new();
+    let binding = fs::read_to_string("src/y2024/day1/day1.txt").expect("Failed to read file");
+
+    for line in binding.lines() {
+        let stuff = line.split_whitespace().collect::<Vec<_>>();
+
+        match stuff.get(0) {
+            Some(first) => first_part.push(first.parse::<u32>().unwrap_or(0)),
+            None => println!("No value found"),
+        }
+
+        match stuff.get(1) {
+            Some(second) => second_part.push(second.parse::<u32>().unwrap_or(0)),
+            None => println!("No value found"),
+        }
+    }
+
+    // let mut result = Vec::new();
+    // // too expensive
+    // for idx in 0..first_part.iter().count() {
+    //     let first_part_val = match first_part.get(idx) {
+    //       Some(val) => val,
+    //       None => &0,
+    //     };
+    //     if second_part.contains(first_part_val) {
+    //         result.push(second_part.iter().filter(|x| *x == first_part_val).count() as u32 * first_part_val);
+    //     }
+    // }
+    // result.iter().sum()
+
+     // Build a frequency map for `second_part` to speed up lookups
+     let mut second_part_map = HashMap::new();
+     for &value in &second_part {
+         *second_part_map.entry(value).or_insert(0) += 1;
+     }
+ 
+     // Calculate the similarity score
+     let result: u32 = first_part
+         .iter()
+         .filter_map(|&value| {
+             second_part_map.get(&value).map(|&count| count as u32 * value)
+         })
+         .sum();
+ 
+     result
+}
